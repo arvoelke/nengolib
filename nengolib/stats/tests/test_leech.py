185a2cb3caab98b5464f2447920f1b307ca9f189
arvoelke <arvoelke@gmail.com>
2016-01-25 01:31:56 -0500

    Test similarity of leech points

--- nengolib/stats/tests/test_leech.py
+++ nengolib/stats/tests/test_leech.py
@@ -17,3 +17,8 @@ def test_leech_kissing():
 
     x_cached = leech_kissing()
     assert np.allclose(x, x_cached)
+
+    s = np.random.choice(len(x), 1000, replace=False)
+    # The only vectors in x that have dot product with s > 0.5 are those
+    # from s themselves, and so this is exactly 1 (per vector in s)
+    assert np.all(np.sum(np.dot(x, x[s].T) > 0.5, axis=0) == 1)
