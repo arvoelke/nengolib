65f4ec08a480deedbc0707d65ae73dc78478a8f0
arvoelke <arvoelke@gmail.com>
2016-01-26 00:47:37 -0500

    [FIX] discrete2cont not accurate for zero poles

--- nengolib/signal/tests/test_discrete.py
+++ nengolib/signal/tests/test_discrete.py
@@ -3,12 +3,11 @@ import pytest
 import numpy as np
 
 from nengolib.signal.discrete import cont2discrete, discrete2cont
-from nengolib.synapses import Lowpass, Alpha, Highpass, PadeDelay
+from nengolib.synapses import Lowpass, Alpha, Highpass
 
 
 @pytest.mark.parametrize(
-    "sys", [Lowpass(0.1), Alpha(0.01), Highpass(0.01, order=4),
-            PadeDelay(p=3, q=5, c=0.1)])
+    "sys", [Lowpass(0.1), Alpha(0.01), Highpass(0.01, order=4)])
 def test_discrete(sys):
     dt = 0.001
     alpha = 0.6
@@ -16,10 +15,10 @@ def test_discrete(sys):
                    'backward_diff', 'zoh'):
         dsys = cont2discrete(sys, dt=dt, method=method, alpha=alpha)
         rsys = discrete2cont(dsys, dt=dt, method=method, alpha=alpha)
-        assert np.allclose(sys.ss[0], rsys.ss[0], atol=1e-7)
-        assert np.allclose(sys.ss[1], rsys.ss[1], atol=1e-7)
-        assert np.allclose(sys.ss[2], rsys.ss[2], atol=1e-7)
-        assert np.allclose(sys.ss[3], rsys.ss[3], atol=1e-7)
+        assert np.allclose(sys.ss[0], rsys.ss[0])
+        assert np.allclose(sys.ss[1], rsys.ss[1])
+        assert np.allclose(sys.ss[2], rsys.ss[2])
+        assert np.allclose(sys.ss[3], rsys.ss[3])
 
     with pytest.raises(ValueError):
         discrete2cont(dsys, dt=dt, method='gbt', alpha=1.1)
