244557f0d5a13c02f76422614c1465b7bc9f989a
arvoelke <arvoelke@gmail.com>
2016-03-02 23:08:45 -0500

    [TST] Improved testing of PerfectLIF

--- nengolib/neurons.py
+++ nengolib/neurons.py
@@ -10,20 +10,23 @@ class PerfectLIF(LIF):
         # reduce all refractory times by dt
         refractory_time -= dt
 
-        # compute effective dt for each neuron, based on remaining time
+        # compute effective dt for each neuron, based on remaining time.
+        # note that refractory times that have completed midway into this
+        # timestep will be given a partial timestep, and moreover these will
+        # be subtracted to zero at the next timestep (or reset by a spike)
         delta_t = (dt - refractory_time).clip(0, dt)
 
         # update voltage using discretized lowpass filter
-        # since v(dt) = v(0) + (J - v(0))*(1 - exp(-dt/tau)) assuming
+        # since v(t) = v(0) + (J - v(0))*(1 - exp(-t/tau)) assuming
         # J is constant over the interval [t, t + dt)
         voltage -= (J - voltage) * np.expm1(-delta_t / self.tau_rc)
 
-        # determine which neurons spike (if v > 1 set spiked = 1/dt, else 0)
-        spiked_mask = voltage > 1
+        # determine which neurons spiked (set them to 1/dt, else 0)
+        spiked_mask = voltage >= 1
         spiked[:] = spiked_mask / dt
         spiked_v = voltage[spiked_mask]
 
-        # set v(0) = 1 and solve for dt to compute the spike time
+        # set v(0) = 1 and solve for t to compute the spike time
         t_spike = dt + self.tau_rc * np.log1p(
             -(spiked_v - 1) / (J[spiked_mask] - 1))
 
--- nengolib/tests/test_neurons.py
+++ nengolib/tests/test_neurons.py
@@ -6,27 +6,42 @@ from nengo.utils.numpy import rmse
 from nengolib import Network, PerfectLIF
 
 
-def _test_lif(Simulator, seed, neuron_type, u, dt=0.001, n=5000,
-              synapse=0.005, t=1.0):
+def _test_lif(Simulator, seed, neuron_type, u, dt, n=500, t=2.0):
     with Network(seed=seed) as model:
         stim = nengo.Node(u)
         x = nengo.Ensemble(n, 1, neuron_type=neuron_type)
-        nengo.Connection(stim, x, synapse=synapse)
+        nengo.Connection(stim, x, synapse=None)
         p = nengo.Probe(x.neurons)
 
     sim = Simulator(model, dt=dt)
     sim.run(t)
 
-    expected = nengo.builder.ensemble.get_activities(sim.model, x, [u])
-    actual = (sim.data[p] > 0).sum(axis=0) / t
+    expected = nengo.builder.ensemble.get_activities(sim.model, x, [u]) * t
+    actual = (sim.data[p] > 0).sum(axis=0)
 
-    return rmse(actual, expected)
+    return rmse(actual, expected, axis=0)
 
 
-def test_perfect_lif(Simulator, seed, logger):
-    for u in np.linspace(-1, 1, 6):
-        error_lif = _test_lif(Simulator, seed, nengo.LIF(), u)
-        error_perfect_lif = _test_lif(Simulator, seed, PerfectLIF(), u)
-        logger.info("%s <? %s (ratio=%s)", error_perfect_lif, error_lif,
-                    error_perfect_lif / error_lif)
-        assert error_perfect_lif < error_lif
+def test_perfect_lif_performance(Simulator, seed, logger):
+    for dt in np.linspace(0.0005, 0.002, 3):
+        for u in np.linspace(-1, 1, 6):
+            error_lif = _test_lif(Simulator, seed, nengo.LIF(), u, dt)
+            error_perfect_lif = _test_lif(Simulator, seed, PerfectLIF(), u, dt)
+            logger.info("dt=%s, u=%s: %s <? %s (ratio=%s)",
+                        dt, u, error_perfect_lif, error_lif,
+                        error_perfect_lif / error_lif)
+            assert error_perfect_lif < 1
+            assert error_perfect_lif < error_lif
+            seed += 1
+
+
+def test_perfect_lif_invariance(Simulator, seed):
+    # as long as the simulation time is divisible by dt, the same number of
+    # spikes should be observed
+    t = 1.0
+    errors = []
+    for dt in (0.0001, 0.0005, 0.001, 0.002):
+        assert np.allclose(int(t / dt), t / dt)
+        error = _test_lif(Simulator, seed, PerfectLIF(), 0, dt, t=t)
+        errors.append(error)
+    assert np.allclose(errors, errors[0])
