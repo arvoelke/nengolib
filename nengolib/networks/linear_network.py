5135121ba14b3d3d100ab52c6de118f7e04e025e
Aaron Voelker <arvoelke@gmail.com>
2016-04-03 02:08:59 -0400

    [DOC] cleanup/simplify LinearNetwork

--- nengolib/networks/linear_network.py
+++ nengolib/networks/linear_network.py
@@ -21,37 +21,35 @@ class LinearNetwork(Network):
 
         # Parameter checking
         self.sys = LinearSystem(sys)
+        self.n_neurons = n_neurons
         self.synapse = synapse
         self.dt = dt
-        self.n_neurons = n_neurons
 
         if not is_exp_stable(self.sys):
             warnings.warn("System is not exponentially stable: %s" % self.sys)
 
         # Obtain a normalized state-space representation
         self.sys = canonical(self.sys, controllable=False)
-        A, B, C, D = ss2sim(self.sys, self.synapse, self.dt).ss
+        self.A, self.B, self.C, self.D = ss2sim(
+            self.sys, self.synapse, self.dt).ss
 
         # Attributes
-        self.A, self.B, self.C, self.D = (A, B, C, D)
-        self.size_in = B.shape[1]
-        self.size_state = len(A)
-        self.size_out = len(C)
+        self.size_in = self.B.shape[1]
+        self.size_state = len(self.A)
+        self.size_out = len(self.C)
 
         # Create internal Nengo objects
         self.input = nengo.Node(size_in=self.size_in, label="input")
         self.output = nengo.Node(size_in=self.size_out, label="output")
         self.x = nengo.networks.EnsembleArray(
             self.n_neurons, self.size_state, ens_dimensions=1, **ens_kwargs)
-        x_in = self.x.input
-        x_out = self.x.output
 
         # Connect everything up using (A, B, C, D)
         self.conn_A = nengo.Connection(
-            x_out, x_in, transform=A, synapse=synapse)
+            self.x.output, self.x.input, transform=self.A, synapse=synapse)
         self.conn_B = nengo.Connection(
-            self.input, x_in, transform=B, synapse=synapse)
+            self.input, self.x.input, transform=self.B, synapse=synapse)
         self.conn_C = nengo.Connection(
-            x_out, self.output, transform=C, synapse=None)
+            self.x.output, self.output, transform=self.C, synapse=None)
         self.conn_D = nengo.Connection(
-            self.input, self.output, transform=D, synapse=None)
+            self.input, self.output, transform=self.D, synapse=None)
