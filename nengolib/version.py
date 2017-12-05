a7aaee08446b3031201488ced95500c86971c822
arvoelke <arvoelke@gmail.com>
2017-12-05 17:16:56 -0500

    [META] Starting development of 0.4.2

--- nengolib/version.py
+++ nengolib/version.py
@@ -1,6 +1,6 @@
 """Nengo Library version information."""
 
-version_info = (0, 4, 1)  # (major, minor, patch)
-release_type = ""
+version_info = (0, 4, 2)  # (major, minor, patch)
+release_type = "-dev"
 
 version = "%s%s" % (".".join(map(str, version_info)), release_type)
