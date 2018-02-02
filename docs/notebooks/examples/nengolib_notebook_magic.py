9a02537d3f10b436d403c3895a64d5a3000278cb
arvoelke <arvoelke@gmail.com>
2018-02-02 00:25:12 -0500

    [WIP] Somewhat complete draft

--- docs/notebooks/examples/nengolib_notebook_magic.py
+++ docs/notebooks/examples/nengolib_notebook_magic.py
@@ -2,7 +2,9 @@ from IPython.display import Markdown as md
 
 import numpy as np
 
-def latex_tf(sys, pre="H(s) = "):
+def latex_tf(sys, pre="H(%s) = "):
+    sym = "s" if sys.analog else "z"
+    pre = "H(%s) = " % sym
     def latex_poly(poly):
         def latex_coef(c, i):
             if np.allclose(c, int(c)):
@@ -12,8 +14,8 @@ def latex_tf(sys, pre="H(s) = "):
             if c == 1:
                 c = ""
             if i == 1:
-                return "%s s" % (c,)
-            return "%s s^%i" % (c, i)
+                return "%s %s" % (c, sym)
+            return "%s %s^%i" % (c, sym, i)
         return " + ".join(latex_coef(c, len(poly)-i) for i, c in enumerate(poly)
                           if not np.allclose(c, 0))
     return md(r"$%s\frac{%s}{%s}$" % (
